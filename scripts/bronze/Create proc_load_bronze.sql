6ba272a6fd5f9cc98ac0ccfa3b8f4fe612b432fe
MujtabaAliAnsari <mujtabaaliansari@yahoo.com>
2025-12-27 17:04:01 +0500

    Update proc_load_bronze.sql by removing schema creation
    Removed database and schema creation queries from the stored procedure documentation.

--- scripts/bronze/Create proc_load_bronze.sql
+++ scripts/bronze/Create proc_load_bronze.sql
@@ -13,18 +13,6 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 */
 
---Run the below query to create a new database call DATAWAREHOUSE
-CREATE DATABASE datawarehouse;
-
---Run the below query to create a schema which will be the bronze layer if it does not exist already
-CREATE SCHEMA IF NOT EXISTS BRONZE;
-
---Run the below query to create a schema which will be the bronze layer if it does not exist already
-CREATE SCHEMA IF NOT EXISTS SILVER;
-
---Run the below query to create a schema which will be the bronze layer if it does not exist already
-CREATE SCHEMA IF NOT EXISTS GOLD;
-
 DROP PROCEDURE BRONZE.load_bronze();
 
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
